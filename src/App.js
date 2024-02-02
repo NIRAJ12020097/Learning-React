0cd72ddb6991d3f96899f519619592fa73551bc1
NIRAJ12020097 <nirajkumarpanda10@gmail.com>
2024-02-03 02:35:20 +0530

    Bio html Added

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <h1>Niraj Kumar Panda</h1>
+        <p><em>B-Tech in CSE from <strong>Lovely Professional University</strong></em></p>
+        <p>Getting started with my Full Stack Web Development Journey.
+            Hope to acquire the best out of it.</p>
+        <h3>Educational Qualifications</h3>
+        <ul>
+            <li>3rd Year in Computer Science and Engineering , Lovely Professional University</li>
+            <li>Senior Secondary Higher Education - 12th , Kendriya Vidyalaya Berhampur</li>
+            <li>Secondary Higher Education - 10th , Kendriya Vidyalaya Berhampur</li>
+        </ul>
+        <h3>My Achievements and Hobbies</h3>
+        <ol>
+            <li>Badminton(State level)- Present</li>
+            <li>HactoberFest2022</li>
+            <li>Musical Instruments</li>
+        </ol>
+
     </div>
   );
 }
